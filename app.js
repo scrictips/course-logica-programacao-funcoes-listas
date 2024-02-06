5c37612a53fd5352f0edc4f8c86a1033766b4395
scrictips <158336037+scrictips@users.noreply.github.com>
2024-02-05 23:48:17 -0300

    texto novo

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
+let novoTexto = 'isso ae';
 
 function exibiTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
